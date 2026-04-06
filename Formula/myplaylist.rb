b575c30e833d74b2f9f9dfe29a46d9497ba63da2
eddie <edolovee@gmail.com>
2026-04-06 21:45:50 +0800

    chore: update Homebrew Formula to v0.2.0

--- Formula/myplaylist.rb
+++ Formula/myplaylist.rb
@@ -1,9 +1,9 @@
 class Myplaylist < Formula
   desc "AI-powered music playlist generator and player"
   homepage "https://github.com/haoziwlh/autoplaylist"
-  url "https://files.pythonhosted.org/packages/py3/m/myplaylist/myplaylist-0.1.0-py3-none-any.whl",
+  url "https://files.pythonhosted.org/packages/02/2d/d7b47a75d89f362dd8baf0ec4bc456a717d8e3d1596441c4ccb7b307318b/myplaylist-0.2.0-py3-none-any.whl",
       using: :nounzip
-  sha256 "1c0867f4d80e5aa21667a235c3d04c2cfb32a031e11a58991a5d33a329ea6950"
+  sha256 "283bb1d0f6ddefeb0ba79a26065335f9aeb4a54be6ef1d61c7296f697df602d7"
   license "MIT"
 
   depends_on "python@3.11"
